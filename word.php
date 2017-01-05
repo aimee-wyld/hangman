b5ff946f3ad858a16d456ee6d7245a7c34dceed9
jessica <j.j.isaacs@hotmail.co.uk>
2017-01-05 22:17:58 +0000

    moved Word class to class file in classes folder

---
+++ word.php
@@ -0,0 +1,20 @@
+<?php
+require 'classes/Word.php';
+
+$my_word = new Word;
+$response = [];
+
+switch($_POST['request']) {
+    case null:
+        $response['data'] = 'empty';
+        break;
+    case 'getWord':
+        $response['data']['word'] = $my_word->getWord();
+        $response['data']['len'] = $my_word->getWordLength();
+        break;
+    case 'matchLetters':
+        $response['data'] = $my_word->matchLetters($_POST['letter'], $_POST['word']);
+        break;
+}
+$response = json_encode($response['data']);
+echo $response;
