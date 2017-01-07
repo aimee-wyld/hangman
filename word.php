d34c7c7ebc6b53841d70d136836b5a53282b641c
jessica <j.j.isaacs@hotmail.co.uk>
2017-01-07 18:56:05 +0000

    class Word: make library var a construct param

--- word.php
+++ word.php
@@ -1,7 +1,18 @@
 <?php
 require 'classes/Word.php';
 
-$my_word = new Word();
+$library = ['doctor', 'quixotic', 'grease', 'enchanted', 'thundering', 'plant', 'caring', 'mysterious',
+    'embarrass', 'cause', 'chivalrous', 'brick', 'ordinary', 'daffy', 'dazzling', 'hate', 'oval', 'add', 'rural',
+    'disillusioned', 'hellish', 'nondescript', 'wealth', 'realise', 'legs', 'rule', 'interfere', 'malicious',
+    'thirsty', 'uttermost', 'plants', 'rapid', 'care', 'hurry', 'develop', 'tendency', 'puny', 'sturdy', 'unwieldy',
+    'weary', 'elite', 'furry', 'lock', 'sparkling', 'lackadaisical', 'carriage', 'basketball', 'pocket', 'tongue',
+    'curve', 'teeny-tiny', 'wonder', 'wrestle', 'turn', 'short', 'awesome', 'snotty', 'yummy', 'button', 'humorous',
+    'shock', 'secret', 'statement', 'onerous', 'unaccountable', 'skinny', 'amazing', 'nappy', 'judicious', 'toad',
+    'laughable', 'hard', 'regret', 'jog', 'suit', 'business', 'pies', 'wave', 'raspy', 'dog', 'buzz', 'need', 'man',
+    'route', 'zippy', 'sour', 'flat', 'scarce', 'committee', 'abstracted', 'cure', 'money', 'innate', 'pets', 'machine',
+    'cattle', 'comfortable', 'tidy', 'spark', 'wooden', 'trip'];
+
+$my_word = new Word($library);
 $response = [];
 
 switch($_POST['request']) {
